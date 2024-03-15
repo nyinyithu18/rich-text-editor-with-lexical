997bcce671a682f2470f4828e98d0151567905b1
Nyi Nyi Thu <nyithu185@gmail.com>
2024-03-15 20:18:31 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { CssBaseline, Grid, ThemeProvider, Typography } from "@mui/material";
+import LexicalEditorWrapper from "./components/LexicalEditorWrapper";
+import theme from "./theme";
+import "./App.css";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid
+        container
+        sx={{ minHeight: "100vh" }}
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Grid item sx={{ my: 4 }}>
+          <Typography variant="h4">Lexical Editor</Typography>
+        </Grid>
+        <Grid item sx={{ width: 750, overflow: "hidden" }}>
+          <LexicalEditorWrapper />
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
+
+export default App;
